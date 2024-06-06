8184b2734340730ebc5d4c41a4a30e57a3483a06
Johan <johan.perez05@gmail.com>
2024-06-06 21:35:05 +0200

    add the burger menu

--- assets/react/components/Header.jsx
+++ assets/react/components/Header.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Header() {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    };
+
     return (
         <>
-        <header>
-            <h1><a className='link' href="/">V.PARROT</a></h1>
-            <nav>
-                <ul>
-                    <li><a className='link' href="/">Accueil</a></li>
-                    <li><a className='link' href="/About">A propos</a></li>
-                    <li><a className='link' href="/Services">Entretiens & Réparations</a></li>
-                    <li><a className='link' href="/SecondHand">Occasions</a></li>
-                    <li><a className='link' href="/Contact">Contact</a></li>
-                    <li><a className='link' href="/Login">Connexion</a></li>
-                </ul>
-            </nav>
-        </header>
+            <header>
+                <h1><a className='link' href="/">V.PARROT</a></h1>
+                <nav className={isActive ? 'active' : ''}>
+                    <ul>
+                        <li><a className='link' href="/">Accueil</a></li>
+                        <li><a className='link' href="/About">A propos</a></li>
+                        <li><a className='link' href="/Services">Entretiens & Réparations</a></li>
+                        <li><a className='link' href="/SecondHand">Occasions</a></li>
+                        <li><a className='link' href="/Contact">Contact</a></li>
+                        <li><a className='link' href="/Login">Connexion</a></li>
+                    </ul>
+                </nav>
+                <button
+                    type='button'
+                    aria-label='toggle curtain navigation'
+                    className={`nav-toggler ${isActive ? 'active' : ''}`}
+                    onClick={toggleMenu}
+                >
+                    <span className="line l1"></span>
+                    <span className="line l2"></span>
+                    <span className="line l3"></span>
+                </button>
+            </header>
         </>
     );
 }
