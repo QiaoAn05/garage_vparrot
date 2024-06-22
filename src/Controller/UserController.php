19126985f480891516f158c19545905b18c5cbe4
Johan <johan.perez05@gmail.com>
2024-06-22 14:36:40 +0200

    add the password hasher to the create to register a user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface as SerializerSerializerInterface;
 
@@ -24,15 +25,22 @@ class UserController extends AbstractController
     }
 
     #[Route('/user/create', name: 'user_create', methods:['POST'])]
-    public function createUser(EntityManagerInterface $em, Request $request): JsonResponse
+    public function createUser(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
         $user = new User();
         $user->setUsername($data['username']);
         $user->setRoles($data['role']);
-        $user->setPassword($data['password']);
-   
+
+        $plaintextPassword = $data['password'];
+
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+
+        $user->setPassword($hashedPassword);   
     
         // Persist et flush
         $em->persist($user);
